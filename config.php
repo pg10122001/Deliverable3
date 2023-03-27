97717e376097136202629ff703d44818d66f6c4f
pg10122001 <122991788+pg10122001@users.noreply.github.com>
2023-03-27 19:39:11 -0300

    Project file #1 commit

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+$config = array(
+    'smarty_dir' => 'smarty3/'
+    );
+>
+
+// Notice there's no end to this tag.  This is normal for library files
+// and other php files you include.  PHP will allow this, and having it
+// there can accidentally trigger an error
